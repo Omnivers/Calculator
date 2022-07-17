f5adf1accd9972e311f61d591517f1447e713c99
Omnivers <Twersimor.stock@outlook.com>
2022-07-17 20:08:48 +0200

    Calculator html/Css introduction

--- src/App.js
+++ src/App.js
@@ -3,21 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container row">
+      <div className="output">
+        <div className="previous-operand">
+          45094905
+        </div>
+        <div className="current-operand">
+          *9304829
+        </div>
+      </div>
+      <div className="row">
+      <button className="span-two col">AC</button>
+      <button className='col'>DEL</button>
+      <button className='col'>÷</button>
+      </div>
+      <div className="row">
+      <button className='col'>1</button>
+      <button className='col'>2</button>
+      <button className='col'>3</button>
+      <button className='col'>*</button>
+      </div>
+      <div className="row">
+      <button className='col'>4</button>
+      <button className='col'>5</button>
+      <button className='col'>6</button>
+      <button className='col'>+</button>
+      </div>
+      <div className="row">
+      <button className='col'>7</button>
+      <button className='col'>8</button>
+      <button className='col'>9</button>
+      <button className='col'>-</button>
+      </div>
+      <div className="row">
+      <button className='col'>.</button>
+      <button className='col'>0</button>
+      <button className='col span-two'>=</button>
+      </div>
     </div>
   );
 }
